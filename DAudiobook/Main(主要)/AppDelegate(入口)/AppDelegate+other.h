a499299bed5f9636e73872b84f78bfb5adb72313
肖俊 <xiaojun@sinog2c.com>
2019-11-05 17:18:33 +0800

    bug  fix

--- DAudiobook/Main(主要)/AppDelegate(入口)/AppDelegate+other.h
+++ DAudiobook/Main(主要)/AppDelegate(入口)/AppDelegate+other.h
@@ -11,6 +11,8 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface AppDelegate (other)
+
+- (void)registerAPNs;
 - (void)registerAPNS:(UIApplication *)application launchOptions:(NSDictionary *)launchOptions;
 
 @end
